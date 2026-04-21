35f1ae75de3667eaa2e0a351743828612a4fc28c
ericq1n <ericq1357@gmail.com>
2026-04-21 11:43:25 -0500

    Deleted package dependencies. You must run "npm install" each pull now, but will prevent 403 errors.

--- backend/dist/utils/validators.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function validateEmail(email: string): boolean;
-export declare function validateAmount(amount: unknown): boolean;
-export declare function validateAccountType(type: unknown): boolean;
-export declare function validatePhone(phone: unknown): boolean;
-export declare function validateZipCode(zip: unknown): boolean;
-export declare function validateDateOfBirth(dob: unknown): boolean;
-export declare function validateState(state: unknown): boolean;
-//# sourceMappingURL=validators.d.ts.map
